d15694814d63e95e5f4d4372078a09ff5f702a36
chenqh19 <chenqh23@mit.edu>
2025-11-23 18:44:32 -0800

    changed hard-coding dir

--- examples/SQL+DF-Examples/micro-benchmarks/notebooks/accel-support.sh
+++ examples/SQL+DF-Examples/micro-benchmarks/notebooks/accel-support.sh
@@ -1,12 +1,12 @@
 sudo chgrp sudo /dev/dsa/wq*
 sudo chmod 660 /dev/dsa/wq*
 
-sudo -E env HOME=/home/chenqh23 \
-  SPARK_HOME=/home/chenqh23/spark/dist \
-  QPL_INCLUDE=/home/chenqh23/qpl_install_dir/include \
-  QPL_LIB=/home/chenqh23/qpl_install_dir/lib \
-  LD_LIBRARY_PATH=/home/chenqh23/offload/iaa-compress/native/build:/home/chenqh23/qpl_install_dir/lib:$LD_LIBRARY_PATH \
+sudo -E env HOME=$HOME \
+  SPARK_HOME=$HOME/spark/dist \
+  QPL_INCLUDE=$HOME/qpl_install_dir/include \
+  QPL_LIB=$HOME/qpl_install_dir/lib \
+  LD_LIBRARY_PATH=$HOME/offload/iaa-compress/native/build:$HOME/qpl_install_dir/lib:$LD_LIBRARY_PATH \
   QPL_PATH=hardware \
-  PATH=/home/chenqh23/venv/sparkrapids/bin:$PATH \
-  /home/chenqh23/venv/sparkrapids/bin/python \
-  /home/chenqh23/spark-rapids-examples/examples/SQL+DF-Examples/micro-benchmarks/notebooks/test_iaa.py
+  PATH=$HOME/venv/sparkrapids/bin:$PATH \
+  $HOME/venv/sparkrapids/bin/python \
+  $HOME/spark-rapids-examples/examples/SQL+DF-Examples/micro-benchmarks/notebooks/test_iaa.py
