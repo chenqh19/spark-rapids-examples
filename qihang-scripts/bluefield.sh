94d997666cdfa211d913ac9c7700171106e1ec02
chenqh23 <chenqh23@localhost>
2025-11-30 18:28:54 -0500

    made dpdk work

--- qihang-scripts/bluefield.sh
+++ qihang-scripts/bluefield.sh
@@ -26,23 +26,15 @@ sudo modprobe mlx5_core
 sudo modprobe mlx5_ib
 
 # put required packages to smartnic
-mkdir -p ~/bf_wheels
 sudo apt install python3-pip
-pip3 download \
-  --dest ~/bf_wheels \
-  --only-binary=:all: \
-  --implementation cp \
-  --platform manylinux2014_aarch64 \
-  --python-version 310 \
-  meson ninja
-sudo dpkg --add-architecture arm64
-echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports jammy main universe" | \
-    sudo tee /etc/apt/sources.list.d/jammy-arm64.list
-sudo apt-get update
-sudo apt-get download libjson-c-dev:arm64 libjson-c5:arm64
-scp libjson-c5_*arm64.deb libjson-c-dev_*arm64.deb ubuntu@192.168.100.2:/tmp/
 
-scp -r ~/bf_wheels ubuntu@192.168.100.2:/tmp/wheels # mkdir -p /tmp/wheels/ on smartnic first
+sudo dpkg --add-architecture arm64
+echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports jammy main universe' | \
+  sudo tee /etc/apt/sources.list.d/jammy-arm64.list
+sudo apt-get update
+mkdir -p ~/dpu_debs && cd ~/dpu_debs
+sudo apt-get download ninja-build:arm64 meson
+scp ~/dpu_debs/ninja-build_*arm64.deb ~/dpu_debs/meson_*all.deb ubuntu@192.168.100.2:/tmp/
 
 # dpdk
 sudo apt update
@@ -63,13 +55,32 @@ grep -i Huge /proc/meminfo | egrep 'HugePages_Total|HugePages_Free'
 
 ### ON SMARTNIC ###
 # dpdk
+sudo dpkg -i /tmp/ninja-build_*arm64.deb
+sudo dpkg -i /tmp/meson_*all.deb
+meson --version && ninja --version
+
 set -e
 cd ~/dpdk
 git checkout v20.11
 rm -rf build
-meson setup --wipe build
-meson setup build --buildtype=release -Dibverbs_link=dlopen
-ninja -C build 
+# /usr/bin/meson setup ~/dpdk/build --buildtype=release -Ddefault_library=shared -Dibverbs_link=dlopen
+# ninja -C ~/dpdk/build
+unset PYTHONPATH; sudo -E /usr/bin/meson install -C ~/dpdk/build
+sudo ldconfig
+
+find /usr/local -maxdepth 5 -name 'librte_*mlx5*' -print
+echo 1024 | sudo tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+sudo mkdir -p /mnt/huge && sudo mount -t hugetlbfs -o pagesize=2M nodev /mnt/huge
+# test
+export MLX5_GLUE_PATH=/usr/local/lib/aarch64-linux-gnu/dpdk/pmds-21.0-glue/librte_common_mlx5_glue.so.21.0
+sudo dpdk-testpmd -l 0-1 -n 4 -m 1024 \
+  -d /usr/local/lib/aarch64-linux-gnu/dpdk/pmds-21.0/librte_common_mlx5.so \
+  -d /usr/local/lib/aarch64-linux-gnu/dpdk/pmds-21.0/librte_net_mlx5.so \
+  -d /usr/local/lib/aarch64-linux-gnu/dpdk/pmds-21.0/librte_mempool_ring.so \
+  -a 0000:03:00.0 -a 0000:03:00.1 \
+  -- --txq=1 --rxq=1 --hairpinq=1 --hairpin-mode=0x01 \
+     --forward-mode=hairpin --auto-start --stats-period=2
+
 
 
 # doca
@@ -77,6 +88,4 @@ python3 -m ensurepip --upgrade || true
 python3 -m pip install --no-index --find-links /tmp/wheels meson ninja
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
-meson --version
-ninja --version
 sudo dpkg -i /tmp/libjson-c5_*arm64.deb /tmp/libjson-c-dev_*arm64.deb || sudo apt-get -f install
